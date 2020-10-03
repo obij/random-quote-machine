7033a456d9f6c9462206be06e59c5910752abd59
Obinna Okafor <okaforobinna7@gmail.com>
2020-10-03 22:29:14 +0100

    Updates

---
+++ precache-manifest.54e998537ba9327ce70c288b4a7aec1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69e081f4c8dea0dd2779026b9a2de9c",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "7b1030cb983d37dcfa92",
+    "url": "/random-quote-machine/static/css/main.132045ee.chunk.css"
+  },
+  {
+    "revision": "c21881fb51104fb845ca",
+    "url": "/random-quote-machine/static/js/2.06cb008b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.06cb008b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1030cb983d37dcfa92",
+    "url": "/random-quote-machine/static/js/main.dfb6c568.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
